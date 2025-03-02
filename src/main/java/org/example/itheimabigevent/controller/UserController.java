024c2de7a338e1fbe90ec0d52ed7d41c46e15d05
A1loha666 <292081295@qq.com>
2025-03-02 08:33:06 +0800

    login

--- src/main/java/org/example/itheimabigevent/controller/UserController.java
+++ src/main/java/org/example/itheimabigevent/controller/UserController.java
@@ -1,8 +1,10 @@
 package org.example.itheimabigevent.controller;
 
+import jakarta.validation.constraints.Pattern;
 import org.example.itheimabigevent.pojo.Result;
 import org.example.itheimabigevent.pojo.User;
 import org.example.itheimabigevent.service.UserService;
+import org.example.itheimabigevent.utils.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,9 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username,String password) {
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
 
-        //search
+        //searchw
         User u = userService.findByUserName(username);
         if(u==null) {
             //register
@@ -28,6 +30,18 @@ public class UserController {
             //repeat
             return Result.error("用户名已被占用");
         }
+    }
 
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password){
+
+        User LoginUser = userService.findByUserName(username);
+        if(LoginUser==null) {
+            return Result.error("用户名不存在！");
+        }
+        if(LoginUser.getPassword().equals(MD5Util.getMD5String(password))) {
+            return Result.success("jwt token");
+        }
+         return Result.error("密码错误！");
     }
 }
